47db8b4b4b6830596c1f106aaaac194fd6911f0d
Juan Pablo <jpbaena13@gmail.com>
2017-06-14 14:32:39 -0500

    Trayendo comentarios

--- source/pages/containers/Home.jsx
+++ source/pages/containers/Home.jsx
@@ -15,6 +15,8 @@ class Home extends Component {
 			posts: [],
 			loading: true,
 		}
+
+		this.handleScroll = this.handleScroll.bind(this);
 	}
 
 	async componentDidMount() {
@@ -24,7 +26,43 @@ class Home extends Component {
 			posts,
 			page: this.state.page + 1,
 			loading: false
-		})
+		});
+
+		window.addEventListener('scroll', this.handleScroll);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll)
+	}
+
+	handleScroll(event) {		
+		console.log(window.scrollY);
+		return null;
+		if (this.state.loading) return null;
+
+		const scrolled = window.scrollY;
+		const viewportHeight = window.innerHeight;
+		const fullHeight = document.body.clientHeight;
+
+		if ( !(scrolled + viewportHeight + 300 >= fullHeight) ) {
+			return null;
+		}
+
+		this.setState({ loading: true },
+			async () => {
+				try {
+					const posts = await api.posts.getList(this.state.page);
+					this.setState({
+						posts: this.state.posts.concat(posts),
+						page: this.state.page + 1,
+						loading: false,
+					})
+				} catch (error) {
+					console.log(error)
+					this.setState({loading: false})
+				}
+			}
+		)
 	}
 
 	render() {
